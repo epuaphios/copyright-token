a869b38917c2677df47d3223dca73a24c650c8a2
ognn <ogunozalp1991@gmail.com>
2023-04-20 23:47:12 +0300

    test

--- src/transfer.ts
+++ src/transfer.ts
@@ -6,11 +6,10 @@
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-import { AptosClient, AptosAccount, CoinClient, FaucetClient } from "aptos";
-import { NODE_URL, FAUCET_URL } from "./common";
-import { readFile } from 'fs/promises';
-import { load } from 'js-yaml';
-
+import {AptosClient, AptosAccount, CoinClient, FaucetClient,HexString} from "aptos";
+import {NODE_URL, FAUCET_URL} from "./common";
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 
 (async () => {
@@ -18,61 +17,67 @@ import { load } from 'js-yaml';
     // :!:>section_1
     const client = new AptosClient(NODE_URL);
     const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL); // <:!:section_1
+    const fileContents = fs.readFileSync('.aptos/config.yaml', 'utf8');
+    const data: any = yaml.load(fileContents);
+    // console.log(data);
+    const privateKey = data.profiles.default.private_key;
+    // console.log(privateKey);
+
 
     // Create client for working with the coin module.
     // :!:>section_1a
     const coinClient = new CoinClient(client); // <:!:section_1a
 
+    const hexObj = new HexString(privateKey);
+    const alice = new AptosAccount(hexObj.toUint8Array());
+
+    console.log(alice.address()); // account adresini yazdir
+
+    const accountAddress = "0x89576037b3cc0b89645ea393a47787bb348272c76d6941c574b053672b848039";
+
     // Create accounts.
     // :!:>section_2
-    const alice = new AptosAccount();
-    const configFile = ".aptos/config.yaml";
+    // const alice = new AptosAccount();
+    // const bob = new AptosAccount(); // <:!:section_2
 
+    // Print out account addresses.
+    console.log("=== Addresses ===");
+    console.log(`Alice: ${alice.address()}`);
+    // console.log(`Bob: ${bob.address()}`);
+    console.log("");
 
+    // Fund accounts.
+    // :!:>section_3
+    await faucetClient.fundAccount(alice.address(), 100_000_000);
+    // await faucetClient.fundAccount(bob.address(), 0); // <:!:section_3
 
-    const yaml = load(await readFile(configFile, "utf8")) as string;
-    console.log(yaml);
+    // Print out initial balances.
+    console.log("=== Initial Balances ===");
+    // :!:>section_4
+    console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
+    // console.log(`Bob: ${await coinClient.checkBalance(bob)}`); // <:!:section_4
+    console.log("");
 
-    // const aptosAccount = new AptosAccount(Buffer.from(privateKey),adress);
-    //
-    // // Print out account addresses.
-    // console.log("=== Addresses ===");
-    // console.log(`Alice: ${aptosAccount.address()}`);
-    // // console.log(`Bob: ${bob.address()}`);
-    // console.log("");
-    //
-    // // Fund accounts.
-    // // :!:>section_3
-    // await faucetClient.fundAccount(alice.address(), 100_000_000);
-    // // await faucetClient.fundAccount(bob.address(), 0); // <:!:section_3
-    //
-    // // Print out initial balances.
-    // console.log("=== Initial Balances ===");
-    // // :!:>section_4
-    // console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
-    // // console.log(`Bob: ${await coinClient.checkBalance(bob)}`); // <:!:section_4
-    // console.log("");
-    //
-    // // Have Alice send Bob some AptosCoins.
-    // // :!:>section_5
-    // let txnHash = await coinClient.transfer(alice, "0x280ba7e93ba82dc3f59e4642a8dea51b4b0278566ffe997429582703367d616a", 1_000, { gasUnitPrice: BigInt(100) }); // <:!:section_5
-    // // :!:>section_6a
-    // await client.waitForTransaction(txnHash); // <:!:section_6a
-    //
-    // // Print out intermediate balances.
-    // console.log("=== Intermediate Balances ===");
-    // console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
-    // console.log(`Bob: ${await coinClient.checkBalance("0x280ba7e93ba82dc3f59e4642a8dea51b4b0278566ffe997429582703367d616a")}`);
-    // console.log("");
-    //
-    // // Have Alice send Bob some more AptosCoins.
-    // txnHash = await coinClient.transfer(alice, "0x280ba7e93ba82dc3f59e4642a8dea51b4b0278566ffe997429582703367d616a", 1_000 );
-    // // :!:>section_6b
-    // await client.waitForTransaction(txnHash, { checkSuccess: true }); // <:!:section_6b
-    //
-    // // Print out final balances.
-    // console.log("=== Final Balances ===");
-    // console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
-    // console.log(`Bob: ${await coinClient.checkBalance("0x280ba7e93ba82dc3f59e4642a8dea51b4b0278566ffe997429582703367d616a")}`);
-    // console.log("");
+    // Have Alice send Bob some AptosCoins.
+    // :!:>section_5
+    let txnHash = await coinClient.transfer(alice, accountAddress, 1_000, {gasUnitPrice: BigInt(100),coinType: "0x1::aptos_coin::AptosCoin"}); // <:!:section_5
+    // :!:>section_6a
+    await client.waitForTransaction(txnHash); // <:!:section_6a
+
+    // Print out intermediate balances.
+    console.log("=== Intermediate Balances ===");
+    console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
+    // console.log(`Bob: ${await coinClient.checkBalance(accountAddress)}`);
+    console.log("");
+
+    // Have Alice send Bob some more AptosCoins.
+    txnHash = await coinClient.transfer(alice, accountAddress, 1_000);
+    // :!:>section_6b
+    await client.waitForTransaction(txnHash, {checkSuccess: true}); // <:!:section_6b
+
+    // Print out final balances.
+    console.log("=== Final Balances ===");
+    console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
+    // console.log(`Bob: ${await coinClient.checkBalance(accountAddress)}`);
+    console.log("");
 })();
